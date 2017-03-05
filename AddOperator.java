79569082a0d3422e26ccc16618f0409bd1995938
Vanishree <vanishreek@gamil.com>
2017-03-04 23:12:08 -0800

    first

---
+++ AddOperator.java
@@ -0,0 +1,13 @@
+package com.githubauto.excercise7;
+
+public class AddOperator implements Operator{
+
+  public int execute(int a, int b) {
+    return a+b;
+    
+  }
+  @Override
+  public String toString(){
+     return "+";
+  }
+}
